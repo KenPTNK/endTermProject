e77dd64b23e43fc4cf1234785fad4dc8985fd40f
KenPTNK <khactrungkiennguyen@gmail.com>
2024-11-10 10:33:53 +0700

    hello

---
+++ script.js
@@ -0,0 +1,19 @@
+// Get the button
+const backToTopButton = document.getElementById("btn-back-to-top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    backToTopButton.style.display = "block"; // Show the button
+  } else {
+    backToTopButton.style.display = "none"; // Hide the button
+  }
+};
+
+// When the user clicks the button, scroll to the top
+backToTopButton.onclick = function() {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth" // Smooth scroll to the top
+  });
+};
