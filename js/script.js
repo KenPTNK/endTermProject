f0854691a307d1ab95edec66087e9167059d04c7
KenPTNK <khactrungkiennguyen@gmail.com>
2024-11-17 12:08:57 +0700

    ndsjkfnjkasd

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 const backToTopButton = document.getElementById("btn-back-to-top");
 
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {
+window.onscroll = function () {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     backToTopButton.style.display = "block"; // Show the button
   } else {
@@ -11,9 +11,19 @@ window.onscroll = function() {
 };
 
 // When the user clicks the button, scroll to the top
-backToTopButton.onclick = function() {
+backToTopButton.onclick = function () {
   window.scrollTo({
     top: 0,
     behavior: "smooth" // Smooth scroll to the top
   });
 };
+
+// Function to show the information //
+function showInfo() {
+  document.querySelector('.info').style.display = 'block';
+}
+
+// Function to hide the information
+function hideInfo() {
+  document.querySelector('.info').style.display = 'none';
+}
