b034ddb659c149e675c242550f1b1c2c8bdfc79a
KenPTNK <khactrungkiennguyen@gmail.com>
2024-11-24 12:22:52 +0700

    login

--- js/main.js
+++ js/main.js
@@ -36,27 +36,26 @@ function signUp() {
   let passwordData = document.getElementById('password').value
   let cfPasswordData = document.getElementById('cfPassword').value
 
+  if (firstNameData === '' || lastNameData === '' || emailData === '' || countryData === '' || passwordData === '') {
+    alert('Please fill all of the gaps')
+    return
+  }
 
   let users = JSON.parse(localStorage.getItem('users')) || []
+
 
   for (let i = 0; i < users.length; i++) {
     if (passwordData !== cfPasswordData) {
       alert('Incorrect password')
       return
     }
-    if (users[i].email === emailData) {
-      alert('Email already exist')
-      return
-    }
-    if (user[i].firstName === firstNameData) {
-      alert('Username already exist')
-      return
-    }
-    if (user[i].lastName === lastNameData) {
-      alert('Username already exist')
-      return
+    if (users[i].email === emailData || users[i].firstName === firstNameData || users[i].lastName === lastNameData) {
+      alert('Account already exist')  
     }
   }
+
+  alert('Sign Up Successfully')
+
   const data = {
     firstName: firstNameData,
     lastName: lastNameData,
@@ -69,4 +68,35 @@ function signUp() {
   users.push(data)
   localStorage.setItem('users', JSON.stringify(users))
 
+}
+
+
+
+function logIn() {
+  event.preventDefault();
+  let firstNameData = document.getElementById('firstName').value
+  let lastNameData = document.getElementById('lastName').value
+  let emailData = document.getElementById('email').value
+  let countryData = document.getElementById('country').value
+  let passwordData = document.getElementById('password').value
+  let cfPasswordData = document.getElementById('cfPassword').value
+  
+  let users = JSON.parse(localStorage.getItem('users')) || []
+  
+  if (firstNameData === '' || lastNameData === '' || emailData === '' || countryData === '' || passwordData === '') {
+    alert('Please fill all of the gaps')
+    return
+  }
+  
+  for (let i = 0; i < users.length; i++) {
+    if (passwordData !== cfPasswordData) {
+      alert('Incorrect Password')
+      return
+    }
+    if (users[i].email === emailData && users[i].firstName === firstNameData && users[i].lastName === lastNameData && users[i].country === countryData && users[i].password === passwordData) {
+      alert('Log in successfully')
+      location.href ="./index.html"
+      return
+    }
+  }
 }
